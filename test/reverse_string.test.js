2ae3013313f4f5f3b3f81016abeab9d488a702f9
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-27 23:46:46 +0530

    Add testing to reverseString  function

--- test/reverse_string.test.js
+++ test/reverse_string.test.js
@@ -4,4 +4,6 @@ test('it should return the reversed string', () => {
   expect(reverseString('hello')).toBe('olleh');
 });
 
-
+it('it should return a reversed sentence', () => {
+  expect(reverseString('Hello World')).toMatch('dlroW olleH');
+});
