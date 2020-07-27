8ba0f9133d488f1f2dfbbfac5a595bbf765ca3a7
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-07-27 23:46:09 +0530

    Add testing to ceaser_cipher  function

---
+++ test/ceaser_cipher.test.js
@@ -0,0 +1,15 @@
+import cipher from '../src/ceaser_cipher';
+
+it('it should return a ciphered word', () => {
+  expect(cipher('abc', 1)).toBe('bcd');
+});
+
+it('it should return a ciphered sentence', () => {
+  expect(cipher('hello world', 3)).toBe('khoor zruog');
+});
+
+it('it should return a ciphered sentence with capital letters', () => {
+  expect(cipher('Hello World', 3)).toBe('Khoor Zruog');
+});
+
+
--- test/reverse_string.test.js
+++ test/reverse_string.test.js
@@ -3,3 +3,5 @@ import reverseString from '../src/reverse_string'
 test('it should return the reversed string', () => {
   expect(reverseString('hello')).toBe('olleh');
 });
+
+
